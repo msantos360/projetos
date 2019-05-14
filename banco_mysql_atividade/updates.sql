24544938ae4c09ce71814c685696d8b4513b27a0
msantos360 <michael.shel96@gmail.com>
2019-05-14 18:47:43 -0300

    Criação do arquivo update para atualização das tabelas

---
+++ banco_mysql_atividade/updates.sql
@@ -0,0 +1,11 @@
+//Atualização na tabela veiculos
+//Campo cliente_fk - acrescentando do proprietário
+update veiculos set cliente_fk = 14587595556 where codigo=2;
+update veiculos set cliente_fk = 36986958548 where codigo=3;
+update veiculos set cliente_fk = 36985847855 where codigo=4;
+update veiculos set cliente_fk = 69585695844 where codigo=5;
+update veiculos set cliente_fk = 36585695844 where codigo=6;
+update veiculos set cliente_fk = 85875695844 where codigo=7;
+update veiculos set cliente_fk = 36285695877 where codigo=8;
+update veiculos set cliente_fk = 44485695874 where codigo=9;
+update veiculos set cliente_fk = 36985095804 where codigo=10;
