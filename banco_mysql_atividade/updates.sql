784ce1bbbf2d542f3652cd39be17050023080591
msantos360 <michael.shel96@gmail.com>
2019-05-15 10:22:39 -0300

    retirando da tabela mao_de_obra a coluna codigo_pecas_fk

--- banco_mysql_atividade/updates.sql
+++ banco_mysql_atividade/updates.sql
@@ -16,15 +16,21 @@ alter table veiculos modify column cliente_fk bigint(11) not null;
 //acrescentando coluna carro a tabela os
 alter table os add column carro_cliente varchar(7);
 
-//inserindo carros a os
-//parei aqui = tem que completar a tabela com o carro do cliente correspondente
+//inserindo as placas dos carros a os
 update os set carro_cliente = 'ZLY3664' where cliente_fk=85875695844;
 update os set carro_cliente = 'ADI8265' where cliente_fk=14587595556;
 update os set carro_cliente = 'TNY2657' where cliente_fk=36986958548;
-update os set carro_cliente = '' where cliente_fk=;
-update os set carro_cliente = '' where cliente_fk=;
-update os set carro_cliente = '' where cliente_fk=;
-update os set carro_cliente = '' where cliente_fk=;
-update os set carro_cliente = '' where cliente_fk=;
-update os set carro_cliente = '' where cliente_fk=;
-update os set carro_cliente = '' where cliente_fk=;
+update os set carro_cliente = 'CAT3006' where cliente_fk=36985847855;
+update os set carro_cliente = 'FMY3661' where cliente_fk=69585695844;
+update os set carro_cliente = 'YXY3663' where cliente_fk=36585695844;
+update os set carro_cliente = 'AUW2785' where cliente_fk=36285695877;
+update os set carro_cliente = 'ARY3668' where cliente_fk=44485695874;
+update os set carro_cliente = 'BOY3666' where cliente_fk=36985095804;
+update os set carro_cliente = 'JUY3665' where cliente_fk=06985695894;
+update os set carro_cliente = 'EDY1652' where cliente_fk=36985695844;
+
+//alterando coluna da tabela 'os' carro_cliente
+alter table os modify column carro_cliente varchar(7) not null;
+
+//retirando da tabela mao_de_obra a coluna codigo_pecas_fk
+alter table mao_de_obra drop column codigo_pecas_fk;
